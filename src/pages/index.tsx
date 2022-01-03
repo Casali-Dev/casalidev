3b8785d904dc94e1d0b44adb21f2ab124f859ab3
Guilherme Casali <guilhermecasali.dev@gmail.com>
2022-01-02 22:52:05 -0300

    fix img styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,11 @@ export default function Home() {
               target="_blank"
               rel="noreferrer"
             >
-              <Image src="images/button-game-image.png" alt="Button Game" />
+              <Image
+                className={styles.img}
+                src="images/button-game-image.png"
+                alt="Button Game"
+              />
             </a>
           </div>
           <div className={styles.gameInfo}>
@@ -36,6 +40,7 @@ export default function Home() {
               rel="noreferrer"
             >
               <Image
+                className={styles.img}
                 src="images/space-code-wip-image.png"
                 alt="Space Code (WIP)"
               />
